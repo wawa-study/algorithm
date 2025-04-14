f837d2546a75d26b25e546a4dc5275ad47616e49
w0nnie <kjw55523@gmail.com>
2025-04-14 13:51:16 +0900

    11053

---
+++ baekjoon/dp/_11053.java
@@ -0,0 +1,29 @@
+package dp;
+
+import java.io.*;
+import java.util.*;
+public class _11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            int a = Integer.parseInt(stk.nextToken());
+            arr[i] = a;
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
